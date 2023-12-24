dd0eef968a21bd7703431eea2d6545c2db5e86f8
Anand S <anandsatheeshk@gmail.com>
2023-12-24 14:52:29 +0530

    Add project files.

---
+++ RedisSample.Api/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using RedisSample.Api.Extensions;
+
+namespace RedisSample.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IDistributedCache _cache;
+
+        public ValuesController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var recordKey = $"Values: {DateTime.Now.ToString("yyyymmdd_hhmm")}";
+            var values = await _cache.GetRecordAsync< List<string>>(recordKey);
+
+            if (values == null)
+            {
+	            await Task.Delay(2000);
+                var dbValues = new List<string>()
+                {
+                    "Anand", "23", "Jason", "Redis", "Docker", "SignalR"
+                };
+
+                await _cache.SetRecordAsync(recordKey, dbValues);
+
+                return Ok(dbValues);
+            }
+
+            return Ok(values);
+        }
+    }
+}
